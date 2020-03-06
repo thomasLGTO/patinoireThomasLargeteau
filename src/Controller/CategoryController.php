3e22c7f332049e168ae59658d2066af391878d1d
thomasLGTO <thomaslargeteau87@gmail.com>
2020-03-06 13:50:57 +0100

    improvement category page and navigation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -21,7 +21,10 @@ class CategoryController extends AbstractController
     public function index(CategoryRepository $categoryRepository): Response
     {
         return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
+            'categories' => $categoryRepository->findBy(
+                [],
+                ['nameCategory' => 'ASC']
+            ),
             'name'=>'Categories',
             'picture'=>'category'
         ]);
