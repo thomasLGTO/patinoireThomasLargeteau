2a358e190b0dde3d6fa1e44d860d40c09d36a377
thomasLGTO <thomaslargeteau87@gmail.com>
2020-03-12 14:29:56 +0100

    creation of a paging system with KnpPaginator

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/categories")
@@ -33,15 +34,20 @@ class CategoryController extends AbstractController
     /**
      * @Route("/{id}", name="category_show", methods={"GET"})
      */
-    public function show(Category $category, CategoryRepository $categoryRepository): Response
+    public function show(Category $category, CategoryRepository $categoryRepository,PaginatorInterface $paginator,Request $request): Response
     {
+        $pagination = $paginator->paginate(
+            $category->getTips(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            2 /*limit per page*/
+        );
         return $this->render('category/show.html.twig', [
             'category' => $category,
-            'tips'=>$category->getTips(),
             'categories' => $categoryRepository->findBy(
                 [],
                 ['nameCategory' => 'ASC']
             ),
+            'pagination' => $pagination
         ]);
     }
 }
